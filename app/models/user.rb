d1e3f20c24cb3305e69452327d6db63d3f52a407
Alia Brown <brown.alia@gmail.com>
2014-04-17 11:24:44 -0700

    changed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
+	has_secure_password
 	has_many :list_items
+	before_create { generate_token(:authentication_token) }
+
+
+
+
+
+	def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+
+  
 end
