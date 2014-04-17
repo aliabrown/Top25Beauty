7693213ffd0424a35403fa444930d7838afdef97
Alia Brown <brown.alia@gmail.com>
2014-04-17 11:29:18 -0700

    changed list_items

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,14 +1,20 @@
 class ListItemsController < ApplicationController
   def index
-  	@list_items = ListItem.all
+  	@list_items = current_user.list_items.all
   end
 
   def new
-  	@list_item = ListItem.new
+  	@list_item = current_user.build_list_items
   end
 
   def create
-  	@list_item = ListItem.create
+  	@list_item = current_user.build_list_items(list_item_params)
   	redirect_to action: 'index'
   end
 end
+
+private
+
+  def list_item_params
+    params.require(:list_item).permit(:user_id, :product_id)
+  end
